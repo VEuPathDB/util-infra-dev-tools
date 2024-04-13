a92ba440bf0acc2482ad1714ae0f94a3b90870e9
Elizabeth <foxcapade@gmail.com>
2024-04-13 01:51:05 -0400

    update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,10 +3,6 @@
 organization=VEuPathDB
 repository=util-vpdb-dev-tool
 
-repoUrl="https://github.com/${organization}/${repository}"
-apiUrl=
-releaseSuffix="/releases/latest"
-
 checkPrereqs() {
   if ! command -v curl; then
     echo "curl is required to run the automatic install script" 1>&2
@@ -15,20 +11,52 @@ checkPrereqs() {
 }
 
 getDownloadURL() {
-  curl -L -H'X-GitHub-Api-Version: 2022-11-28' https://api.github.com/repos/${organization}/${repository}/releases/latest \
+  curl -LsH'X-GitHub-Api-Version: 2022-11-28' https://api.github.com/repos/${organization}/${repository}/releases/latest \
     | grep 'browser_download_url' \
     | grep $1 \
     | grep -o 'http[^"]\+'
 }
 
-case $(uname | tr '[:upper:]' '[:lower:]') in
-  linux*)
+getPathVar() {
+  grep -m1 '^export PATH=' "$1"
+}
 
-    ;;
-  darwin*)
+patchEnvRC() {
+  currentPathValue=$(getPathVar "$1")
+  if ! echo "$currentPathValue" | grep -q "\$HOME/.local/bin|$HOME/.local/bin"; then
+    echo "export PATH=\"\${PATH}:$HOME/.local/bin\"" >> "$1"
+  fi
+}
+
+osName=$(uname | tr '[:upper:]' '[:lower:]')
+
+case $osName in
+  linux|darwin)
+    # do nothing
     ;;
   *)
-    echo "unsupported os" 1>&2
+    echo "unsupported os version" 1>&2
     exit 1
     ;;
 esac
+
+tmpFileName=/tmp/${repository}
+binDir="${HOME}/.local/bin"
+
+curl -Lso ${tmpFileName} "$(getDownloadURL "$osName")"
+mkdir -p "${binDir}"
+unzip ${tmpFileName} -d binDir
+
+trap "rm -f $tmpFileName" EXIT
+
+if command -v vpdb; then
+  exit
+fi
+
+if [ -f "$HOME/.zshrc" ]; then
+  patchEnvRC "$HOME/.zshrc"
+fi
+
+if [ -f "$HOME/.bashrc" ]; then
+  patchEnvRC "$HOME/.bashrc"
+fi
