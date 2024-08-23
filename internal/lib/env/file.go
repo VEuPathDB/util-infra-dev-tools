cdc7f13bf08003a31af3879522bf77d19b378a70
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-08-23 15:25:45 -0400

    code comments

--- internal/lib/env/file.go
+++ internal/lib/env/file.go
@@ -8,6 +8,11 @@ import (
 
 const DotEnvFileName = ".env"
 
+// GetOrCreateDotEnvFile returns a handle on a .env file in the current working
+// directory, creating the file if it does not already exist.
+//
+// This function will panic if an error is encountered while attempting to open
+// or create the file.
 func GetOrCreateDotEnvFile() *os.File {
 	if file, err := os.OpenFile(DotEnvFileName, os.O_CREATE, 0644); err != nil {
 		logrus.Fatalf("failed to open "+DotEnvFileName+" file: %s", err)
